76e1dafacbd24683c41e3a1ed147b5f60aa29d10
Vladislav <neizvesten93@icloud.com>
2026-09-18 14:32:46 +0300

    feat: allow buyers to cancel unpaid orders

--- open_marketplace/web/commerce_order_views.py
+++ open_marketplace/web/commerce_order_views.py
@@ -1,16 +1,20 @@
-from django.views.decorators.http import require_http_methods
+from django.urls import reverse
+from django.views.decorators.http import require_http_methods, require_POST
 
-from open_marketplace.common.errors import InputRejected, PermissionDenied
+from open_marketplace.common.errors import ConcurrentConflict, InputRejected, InvalidState, PermissionDenied
 from open_marketplace.commerce import public as commerce_public
-from open_marketplace.web.identity_views import _context, _login_required
+from open_marketplace.web.identity_views import _context, _login_required, _redirect_303
 
-from .commerce_cart_views import _render
+from .commerce_cart_views import _post_data_is_exact, _render
 
 
 def _error(request, error):
     if isinstance(error, PermissionDenied):
         status = 403
         message = "Заказ недоступен."
+    elif isinstance(error, (ConcurrentConflict, InvalidState)):
+        status = 409
+        message = str(error) or "Состояние заказа уже изменилось."
     else:
         status = 400
         message = str(error) or "Не удалось открыть заказ."
@@ -35,3 +39,14 @@ def commerce_order_detail(request, *, order_id):
     except (PermissionDenied, InputRejected) as error:
         return _error(request, error)
     return _render(request, "commerce_orders/detail.html", {"order": order})
+
+
+@require_POST
+@_login_required
+def commerce_order_cancel(request, *, order_id):
+    try:
+        _post_data_is_exact(request, {"csrfmiddlewaretoken"})
+        commerce_public.cancel_order(order_id=order_id, context=_context(request))
+    except (ConcurrentConflict, InputRejected, InvalidState, PermissionDenied) as error:
+        return _error(request, error)
+    return _redirect_303(reverse("commerce-order-detail", kwargs={"order_id": order_id}))
